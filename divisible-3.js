e83bd0ac07a9abaa1b6e6ce4d348be9c2e42af6e
Rinshad <rinshadcontacts.com>
2025-05-27 10:34:05 +0530

    Divisible-by-3

--- divisible-3.js
+++ divisible-3.js
@@ -1,6 +1,8 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let arr2=[]
 for (i = 0; i < arr.length; i++) {
     if (arr[i]%3!==0) {
-        console.log(arr[i])
+        arr2.push(arr[i])
     }
 }
+console.log(arr2)
