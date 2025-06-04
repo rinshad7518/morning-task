a77c543591bdb2484313d9c9ed8db29304cff378
rinshad7518 <rinshadcontacts@gmail.com>
2025-06-04 15:40:05 +0530

    arrayMethods

--- arrayMethods.js
+++ arrayMethods.js
@@ -2,12 +2,12 @@ const orders=[200,450,130,50,1000,20];
 
 orders.forEach(x=>console.log(x))
 arrFilter=orders.filter(x=>x>100)
-console.log(arrFilter)
+console.log("Filtered Array : ",arrFilter)
 
 arrMap=orders.map(c=>c-10)
-console.log(arrMap)
+console.log("Mapped Array : ",arrMap)
 
-arrReduce=orders.reduce((a,b)=>{
+arrReduce=arrMap.reduce((a,b)=>{
     return a+b
 })
-console.log(arrReduce)
+console.log( "Total Amount After Discount : ",arrReduce)
