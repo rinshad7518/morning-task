75948a73e15f0c27387115b97b0a1040862de3ec
rinshad7518 <rinshadcontacts@gmail.com>
2025-06-10 16:37:08 +0530

    palindome

---
+++ palindrome.js
@@ -0,0 +1,15 @@
+function palindrome(val) {
+    let val2 = String(val)
+    let reversed = ""
+    for (i = val2.length - 1; i >= 0; i--) {
+        reversed += val2[i]
+    }
+    if (val == reversed) {
+        return true
+
+    } else {
+        return false
+    }
+
+}
+console.log(palindrome(-121))
