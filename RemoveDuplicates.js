e892f92738750edcb1828c1e436cd11fe82af6af
rinshad7518 <rinshadcontacts@gmail.com>
2025-06-14 10:49:47 +0530

    RemDuplicates

---
+++ RemoveDuplicates.js
@@ -0,0 +1,25 @@
+arr = [0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 1, 2, 3, 4];
+arr2 = new Set(arr)
+arr3 = [...arr2]
+console.log(arr3)
+
+// function Duplicates(nums) {
+
+//     let dupes = []
+//     if (nums.length === 0) {
+//         return 0
+//     }
+//     for (i = 0; i < nums.length; i++) {
+//         for (j = i + 1; j < nums.length; j++) {
+//             if (nums[i] === nums[j]) {
+//                 dupes.push(nums[i])
+//             }
+//         }
+//     }
+//     return dupes;
+// }
+// console.log(Duplicates([0, 0, 1, 1, 2, 2, 3, 3, 4]))
+
+
+
+
